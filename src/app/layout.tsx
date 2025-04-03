7e3cf5c4b3010846d181d5861138a16face78a9d
oscuro-phoenix <shakulp@mit.edu>
2025-04-03 19:39:38 -0400

    v3.5

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-export const metadata = {
+export const metadata: Metadata = {
   title: 'knotai',
   description: 'Empowering Today\'s Youth to Elevate Tomorrow\'s Workforce',
   icons: {
